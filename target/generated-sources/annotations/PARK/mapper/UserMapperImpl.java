5c554e7ff033190626cb7cb6bf91d9ebd12329a8
Mustafa Ozbalci <mustafa.ozbalci@etiya.com>
2024-07-30 18:55:00 +0300

    MERGE

---
+++ target/generated-sources/annotations/PARK/mapper/UserMapperImpl.java
@@ -0,0 +1,55 @@
+package PARK.mapper;
+
+import PARK.dto.UserDto;
+import PARK.entity.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-30T18:35:24+0300",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Amazon.com Inc.)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto toDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setUserId( user.getUserId() );
+        userDto.setUsername( user.getUsername() );
+        userDto.setPassword( user.getPassword() );
+        userDto.setEmail( user.getEmail() );
+        userDto.setFirstName( user.getFirstName() );
+        userDto.setLastName( user.getLastName() );
+        userDto.setVehiclePlate( user.getVehiclePlate() );
+        userDto.setIsOwner( user.getIsOwner() );
+
+        return userDto;
+    }
+
+    @Override
+    public User toEntity(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setUserId( userDto.getUserId() );
+        user.setUsername( userDto.getUsername() );
+        user.setPassword( userDto.getPassword() );
+        user.setEmail( userDto.getEmail() );
+        user.setFirstName( userDto.getFirstName() );
+        user.setLastName( userDto.getLastName() );
+        user.setVehiclePlate( userDto.getVehiclePlate() );
+        user.setIsOwner( userDto.getIsOwner() );
+
+        return user;
+    }
+}
